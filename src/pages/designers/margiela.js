3d1e4fa4acf2b372abf5aa81ce9dc8d4bcc651c9
shannonharrison118 <shannonharrison118@gmail.com>
2022-11-21 17:26:04 -0500

    adding info to designer pages

---
+++ src/pages/designers/margiela.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './kawakubo.css';
+
+const margiela = () => {
+    return(
+        <div class="page">
+            <h1>Martin Margiela</h1>
+            <p>Martin Margiela is a Belgian fashion designer who founded the French label Maison Margiela. 
+                Margiela has become one of the most iconic designers in recent fashion due to his specific upcycled 
+                aesthetic and deconstructed silhouettes. Despite being one of the biggest names in fashion, Margiela 
+                has never done a face-to-face interview or photograph in order to keep the focus of his brand on the 
+                clothes and designs themselves. Margiela graduated from the Royal Academy of Fine Arts in Antwerp Belgium 
+                in 1979 and is famously part of the fashion collective “Antwerp Six”, known for their collective Avant-Garde 
+                innovative brands. 
+            </p>
+            <p>Before starting his brand, Margiela worked as a freelance designer for five years and then became a design 
+                assistant to Jean Paul Gaultier in Paris until 1987. In 1988, Margiela then created his own label, 
+                Maison Martin Margiela, with a small team of only two other people to help start up the label. Margiela’s 
+                first shows distinguished himself from all other designers of the time. Margiela showed his collections at 
+                various unconventional locations on the outskirts of Paris away from the 1st arrondissement where fashion 
+                shows had almost exclusively taken place in. His first show was in an old Cafe where models walked through 
+                the very dark cafe with the Velvet Underground’s “Guess I’m Falling in Love” blaring throughout the space. 
+                This distinctly different setting for his shows continued throughout his career with one of his most famous 
+                shows taking place in 1989. Margiela had his show take place on a playground in a North African neighborhood 
+                on the outside of Paris. In use of the kid’s playground, Margiela asked them to also hand draw all the invites and 
+                invited the children to sit in the front row of the show and some even walking with and being carried by the models. 
+                This was one of the most famous shows in modern fashion history for the democratizing effect it held at a time 
+                when fashion was revered for its exclusivity. 
+            </p>
+            <p>Margiela continued his career until 2009 where he focused on designing in an upcycling manner. Famously, he created 
+                a guide for people to follow that taught them to sew their own sweater out of old socks. Many of his designs take 
+                a deconstructed approach as well by exposing the seams of garments and using different pieces of various found 
+                garments such as turning a single leather glove into a purse and even making a waistcoat out of old broken fine porcelain plates. 
+            </p>
+            <p>After leaving the eponymous brand in 2099, Margiela did not actively work in the fashion industry anymore and has kept a low 
+                profile since then. In 2019, Margiela helped to produce a documentary outlining his early career and his time at the brand. 
+                In 2021, Margiela also debuted an art exhibition at the Lafayette Anticipations in Paris featuring over 20 different large-scale 
+                sculpture works. The Maison Margiela brand has continued though benign ran by his atelier team following his leaving of the brand 
+                until John Galliano was appointed the creative director in 2014. 
+            </p>
+        </div>
+    )
+}
+
+export default margiela;
---
+++ src/pages/designers/theyskens.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './kawakubo.css';
+
+const theyskens = () => {
+    return(
+        <div class="page">
+            <h1>Olivier Theyskens</h1>
+            <p>Olivier Theyskens is a Belgian fashion designer and is now the owner and designer of his namesake brand. 
+                He went to École Nationale Supérieure des Arts Visuels de la Cambre in Brussels to study Fashion Design 
+                in 1994 and in 1997 he dropped out to start his own label. One of the biggest catalysts of Theyskens’ 
+                career was Madonna wearing one of his pieces to the Academy Awards in 1998. Andre Leon Talley, one of 
+                the most respected fashion journalists and was editor-at-large at Vogue Magazine, listed this dress as 
+                one of his personal favorite oscar dresses of all time in a 2015 article for Vogue. Theyskens’ then presented 
+                his first full collection in Paris in 1998 and his Spring 1999 collection was featured on Vogue in the 25 
+                most unforgettable runway shows of the 90’s. In the year 2000, Theyskens’ was said to be one of the most 
+                powerful and creative personalities in fashion via Vogue and won the Venus de la Mode award. Theyskens’ 
+                continued to work on his line under his own name until 2002 when he then transitioned to the creative 
+                director of Rochas, one of the oldest fashion houses in Paris.
+            </p>
+            <p>Under Rochas, Theyskens’ designed costumes in 2003 for the National Opera House of Belgium, Théâtre Royal de la Monnaie. 
+                Theyskens’  also received the Star Award from the Fashion Group International in 2005. In 2006, Rochas’ fashion line had 
+                been discontinued due to it being the only fashion line under the parent company Procter & Gamble and they did not have 
+                sufficient resources or skills to continue it. 
+            </p>
+            <p>Olivier Theyskens then moved to Nina Ricci where he was appointed the artistic director and changed his focus to 
+                designing a younger, more casual audience. His designs for the Nina Ricci brand were also met with acclaim and praise. 
+                Theyskens’ was artistic director for three years and left the Nina Ricci brand in 2009. Theyskens then moved to Theory 
+                in 2010 and was the artistic director for another four years. He then left the brand in 2014 to pursue various design projects. 
+            </p>
+            <p>In 2016, Theyskens’ went back to designing under his own namesake label and released his first collection since 2002. His return to his 
+                namesake label was entirely self-funded and sought Maximiliano Nicolelli, who worked for LVMH and La Perla, as his Chief Executive. 
+                Theyskens’ has since been designing under his own name and has been the acting artistic director of Azzaro since 2020. 
+            </p>
+        </div>
+    );
+};
+
+export default theyskens;
